0fbc1a1f2ddc25efe26b4f08af5c2f5951d85a14
Brian <brian.lee.1088@gmail.com>
2017-04-22 23:47:23 -0400

    Add results column for queries

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,13 +1,12 @@
 class QueriesController < ApplicationController
   def create
     @query = Query.create(term: params[:query][:term])
+    @query.google_custom_search
     redirect_to @query
   end
 
   def show
-    # binding.pry
     @query = Query.find(params["id"].to_i)
-    @query.google_custom_search
     render "query/show"
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420211941) do
+ActiveRecord::Schema.define(version: 20170423032823) do
 
   create_table "queries", force: :cascade do |t|
     t.string   "term"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.         "results"
   end
 
 end
