7a76f16b1503a8d042325777cc8335ac1ef2b96d
Brian <brian.lee.1088@gmail.com>
2017-04-22 22:58:33 -0400

    Add show page for query results to persist

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,8 +1,18 @@
 class QueriesController < ApplicationController
   def create
     @query = Query.create(term: params[:query][:term])
+    redirect_to @query
+  end
+
+  def show
+    # binding.pry
+    @query = Query.find(params["id"].to_i)
     @query.google_custom_search
     render "query/show"
   end
 
+  # def index
+  #   # binding.pry
+  #   redirect_to root_url
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root "application#index"
-  resources :queries
+  resources :queries, only: [:create, :show]
 end
